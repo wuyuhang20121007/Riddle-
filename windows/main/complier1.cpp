c97080f692221cb2b4d2547ddd4f1cbdc4f013f5
wuyuhang20121007 <163719224+wuyuhang20121007@users.noreply.github.com>
2024-09-29 13:24:24 +0800

    Add files via upload

---
+++ windows/main/complier1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+namespace comp{
+	void cm(char name[]){
+		freopen(name,"r",stdin);
+		string code="",a;
+		while(cin>>a){
+			code+=a;
+		}
+//		system("±àÒëÃüÁî"); 
+	}
+}
+int main(){
+	return 0;
+}
